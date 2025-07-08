173d6bf50a1e2e82161107874b7b3beacab51fb8
rohitragavendra <123528330+rohitragavendra@users.noreply.github.com>
2025-07-08 14:22:12 +0530

    Add files via upload

---
+++ 7th.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(neigh for neigh in graph[node] if neigh not in visited)
+
+# Example Graph (Adjacency List)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# Call BFS
+bfs(graph, 'A')
